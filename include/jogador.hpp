0056605084cbd7275c165513e2d45f3ba734c2f6
luedu1 <118073944+luedu1@users.noreply.github.com>
2022-12-17 19:21:13 -0300

    A

---
+++ include/jogador.hpp
@@ -0,0 +1,17 @@
+#ifndef JOGADOR_H
+#define JOGADOR_H
+
+#include <string>
+#include <iostream>
+
+class Jogador{
+  public:
+  std::string nome;
+  int num_barcos;
+
+  bool validar_barcos();
+};
+
+
+
+#endif
