3129821133775ce78457512a198563987cb10022
Dudz021 <95110541+Dudz021@users.noreply.github.com>
2022-12-17 19:19:09 -0300

    Jogador.hpp

---
+++ src/jogador.hpp
@@ -0,0 +1,21 @@
+#ifndef JOGADOR_H
+#define JOGADOR_H
+
+#include <string>
+#include <iostream>
+
+class Jogador{
+  public:
+  std::string nome;
+  int num_barcos;
+  //std::string Nome;
+  char tabuleiro_ataque[12][24];
+  char tabuleiro_defesa[12][24];
+
+  void get_nome();
+
+};
+
+
+
+#endif
