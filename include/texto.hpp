c5250c33c1272e18d3a9eb5985fdf460d7cdf6f2
Dudz021 <95110541+Dudz021@users.noreply.github.com>
2022-12-20 10:24:05 -0300

    Criação texto.hpp

---
+++ include/texto.hpp
@@ -0,0 +1,13 @@
+#ifndef TEXTO_H
+#define TEXTO_H
+
+#include <string>
+#include <iostream>
+
+void fim_do_jogo();
+
+void boas_vindas();
+
+void setar_linha_barra(std::string& S1, std::string& S2);
+
+#endif
