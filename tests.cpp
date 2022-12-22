0be0f8d48452dd3839edc531bf157a903fb88592
Dudz021 <95110541+Dudz021@users.noreply.github.com>
2022-12-22 14:03:53 -0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,9 +1,3 @@
 #include "doctest.h"
 
-#include "cruzador.h"
-#include "destroyer.h"
-#include "jogador.h"
-#include "navio.h"
-#include "portaviao.h"
-#include "submarino.h"
-#include "tabuleiro.h"
+#include "jogador.hpp"
