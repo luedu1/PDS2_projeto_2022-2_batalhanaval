e04afd61ee11c1304b4c09087e5892ee42a32f51
daniliocs <daniliocs@ufmg.br>
2022-12-22 06:29:08 -0300

    Adição do arquivo inicial de testes

---
+++ tests.cpp
@@ -0,0 +1,9 @@
+#include "doctest.h"
+
+#include "cruzador.h"
+#include "destroyer.h"
+#include "jogador.h"
+#include "navio.h"
+#include "portaviao.h"
+#include "submarino.h"
+#include "tabuleiro.h"
